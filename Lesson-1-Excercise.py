c8e6007ffa98ff517c104f16cfc5609eaaa04f38
Jason Vigil <jdvigil@aurorak12.org>
2024-09-26 14:47:20 -0600

    Completed Excercise 1

--- Lesson-1-Excercise.py
+++ Lesson-1-Excercise.py
@@ -1,2 +1,5 @@
 # Create a variable below called name and assign it your name, 
 # then create a print statement that prints "Hello " + your name variable. 
+
+a = "banana"
+print("I would like a " + a)
